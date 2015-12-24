efe2d187310706774eb1acc7e4961dfff462491d
logicerpsolution <noreply>
2015-12-24 10:05:08 +0000

    Add component registerForm

---
+++ ui/register-form.reel/register-form.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/register-form.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class RegisterForm
+ * @extends Component
+ */
+exports.RegisterForm = Component.specialize(/** @lends RegisterForm# */ {
+    constructor: {
+        value: function RegisterForm() {
+            this.super();
+        }
+    }
+});
